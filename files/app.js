523be773fcaba8a50fa5ee0306e8108166531b47
Justin Ross <jross@apache.org>
2018-07-26 18:00:24 -0700

    Fix the page update time

--- files/app.js
+++ files/app.js
@@ -95,8 +95,7 @@ class Blinky {
     renderFooter(parent) {
         let elem = gesso.createDiv(parent, "footer");
 
-        let offset = new Date().getTimezoneOffset() * 60 * 1000;
-        let time = new Date((this.state.data.update_time - offset));
+        let time = new Date((this.state.data.update_time));
 
         let status = gesso.createSpan(elem, "#timestamp", time.toLocaleString());
 
